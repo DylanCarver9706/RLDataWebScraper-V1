e12328df285083e56c27ef18bff921ef63aa4dc3
Dylan Carver <dylancarver14@gmail.com>
2023-01-15 11:11:54 -0600

    Displays item names only

--- scraper.js
+++ scraper.js
@@ -1,14 +1,18 @@
-// Displays whole page
+// Displays item names only
 
 const express = require('express')
 const request = require('request')
+const cheerio = require('cheerio')
 
 const app = express()
 
 app.get('/', (req, res) => {
     request('https://rocket-league.com/items/shop', (error, response, html) => {
         if (!error && response.statusCode === 200) {
-            res.send(html)
+            const $ = cheerio.load(html)
+            const text1 = $('.rlg-item-shop__name').text()
+            const text2 = $('.rlg-h2.rlg-item-shop__name.--daily').text()
+            res.send("<h1>" + text1 + "</h1>" + "<h1>" + text2 + "</h1>")
         }
     })
 })
@@ -16,3 +20,23 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Server running on port 3000')
 })
+
+
+// Displays whole page
+
+// const express = require('express')
+// const request = require('request')
+
+// const app = express()
+
+// app.get('/', (req, res) => {
+//     request('https://rocket-league.com/items/shop', (error, response, html) => {
+//         if (!error && response.statusCode === 200) {
+//             res.send(html)
+//         }
+//     })
+// })
+
+// app.listen(3000, () => {
+//     console.log('Server running on port 3000')
+// })
