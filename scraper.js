ca34d73939f627279e09989c50c5c7299a6826ce
Dylan Carver <dylancarver14@gmail.com>
2023-01-15 11:26:50 -0600

    Item names separated

--- scraper.js
+++ scraper.js
@@ -1,4 +1,4 @@
-// Displays item names only
+// Displays item names in their own <h1> tags
 
 const express = require('express')
 const request = require('request')
@@ -10,9 +10,12 @@ app.get('/', (req, res) => {
     request('https://rocket-league.com/items/shop', (error, response, html) => {
         if (!error && response.statusCode === 200) {
             const $ = cheerio.load(html)
-            const text1 = $('.rlg-item-shop__name').text()
-            const text2 = $('.rlg-h2.rlg-item-shop__name.--daily').text()
-            res.send("<h1>" + text1 + "</h1>" + "<h1>" + text2 + "</h1>")
+            const h1Tags = $('.rlg-item-shop__name, .rlg-h2.rlg-item-shop__name.--daily')
+            let htmlToPrint = ''
+            h1Tags.each((i, el) => {
+                htmlToPrint += `<h1>${$(el).text()}</h1>`
+            });
+            res.send(htmlToPrint)
         }
     })
 })
@@ -20,6 +23,30 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Server running on port 3000')
 })
+
+
+// Displays item names only
+
+// const express = require('express')
+// const request = require('request')
+// const cheerio = require('cheerio')
+
+// const app = express()
+
+// app.get('/', (req, res) => {
+//     request('https://rocket-league.com/items/shop', (error, response, html) => {
+//         if (!error && response.statusCode === 200) {
+//             const $ = cheerio.load(html)
+//             const text1 = $('.rlg-item-shop__name').text()
+//             const text2 = $('.rlg-h2.rlg-item-shop__name.--daily').text()
+//             res.send("<h1>" + text1 + "</h1>" + "<h1>" + text2 + "</h1>")
+//         }
+//     })
+// })
+
+// app.listen(3000, () => {
+//     console.log('Server running on port 3000')
+// })
 
 
 // Displays whole page
