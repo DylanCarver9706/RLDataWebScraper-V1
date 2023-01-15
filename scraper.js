4f28746483ee5ab1f29b93aa4dd4bd778f00f8ed
Dylan Carver <dylancarver14@gmail.com>
2023-01-15 15:03:59 -0600

    refactor and take notes

--- scraper.js
+++ scraper.js
@@ -1,4 +1,15 @@
 // Displays all text from item <div> tags. Displays color, item, rarity, and random number
+// Next Steps: 
+// 1. Get rid of the numbers using ChatGPT
+//    change the code to where is only displays text from these html elements onto my page in its own <h1> tags, 
+//    but it ignores text that has numbers in it: <div class="rlg-item-shop__item-content --daily"> and <div class="rlg-item-shop__item-content">
+// 2. update the code in ChatGPT to ask it to search Google
+//    using this criteria site:https://rl.insider.gg/en/pc Crimson Fennec ImportBody
+//    The question could go like this:
+//    Update the code to search google.com using this specific search but replace the term Rocket with the results of the web scraper one <h1> tag at a time: 
+//    site:https://rl.insider.gg/en/pc Rocket
+// then click the first link
+// then grab the src link of the photo
 
 const express = require('express')
 const request = require('request')
